f9c0cdb397f46315abc07b657576b4db673d3316
cholekim <nahyeon927@gmail.com>
2021-09-01 12:21:32 +0900

    App4-App12

---
+++ src/App7.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+      <h1>Vue transition 테스트6</h1>
+      <input type="checkbox" v-model="isMake">
+      <transition enter-active-class="animate__animated animate__bounce"
+                  leave-active-class="animate__animated animate__fadeOut"
+                  :duration="1000">
+        <div v-if="isMake" class="box"></div>
+      </transition> 
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  name: 'App7',
+  data(){
+    return {
+      isMake:false
+    };
+  },
+  methods:{
+   
+  }
+}
+</script>
+<style scoped>
+    /* 외부 css 로딩하기 */
+    @import url(https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css);
+</style>
+<style>
+
+  .box{
+    width: 100px;
+    height: 100px;
+    border: 1px solid red;
+    background-color: yellow;
+  }
+
+</style>
